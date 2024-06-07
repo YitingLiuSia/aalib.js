a6bc7d7dc58a0036e4448d2b96193426eafa13a4
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2024-06-07 15:43:57 -0400

    adding pacakges and set up webpack for deployable test on vercel

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,17 +1,40 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     entry: path.resolve(__dirname, "main.js"),
     output: {
         path: __dirname,
         publicPath: path.basename(__dirname) + "/",
-        filename: 'bundle.js'  // Ensure this line is present
-
+        filename: 'bundle.js'
     },
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
     },
-    devtool: "#cheap-module-inline-source-map"
+    devtool: "eval-source-map", // Better for development
+    resolve: {
+        extensions: [".js"]
+    }
 };
+
+if (process.env.NODE_ENV === "production") {
+    module.exports.devtool = "source-map"; // More appropriate for production
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]);
+}
