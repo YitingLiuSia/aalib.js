9abeb6a180207d72da4edc1c319ed9868775db0c
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2024-06-10 11:30:31 -0400

    uploading the main js instead of the playground js and it is working

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -1,40 +1,15 @@
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
     entry: path.resolve(__dirname, "main.js"),
     output: {
         path: __dirname,
-        publicPath: path.basename(__dirname) + "/",
-        filename: 'bundle.js'
+        publicPath: path.basename(__dirname) + "/"
     },
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
     },
-    devtool: "eval-source-map", // Better for development
-    resolve: {
-        extensions: [".js"]
-    }
+    devtool: "#cheap-module-inline-source-map"
 };
-
-if (process.env.NODE_ENV === "production") {
-    module.exports.devtool = "source-map"; // More appropriate for production
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            compress: {
-                warnings: false
-            }
-        }),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        })
-    ]);
-}
