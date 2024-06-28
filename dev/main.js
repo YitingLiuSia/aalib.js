721485bdc114f8dab2186b219898c896bbd6ec24
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2024-06-28 11:25:28 -0400

    update the events in main js

--- dev/main.js
+++ dev/main.js
@@ -17,6 +17,12 @@ const charset_ascii = ASCII_CHARSET;
 const charset_sia = "SIA/.><?!^() ";
 const resource = filename => `../resources/${ filename }`;
 
+window.dataLayer = window.dataLayer || [];
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-1S53JGF525');
 
 // track google events 
 // image download with image size 
