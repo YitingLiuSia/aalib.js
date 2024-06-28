20fbe3a97daaa59d9731202df691436f96fe98e7
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2024-06-28 13:58:34 -0400

    testing with image download again but in triggers for tag manager

--- dev/main.js
+++ dev/main.js
@@ -17,18 +17,11 @@ const charset_ascii = ASCII_CHARSET;
 const charset_sia = "SIA/.><?!^() ";
 const resource = filename => `../resources/${ filename }`;
 
-window.dataLayer = window.dataLayer || [];
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-1S53JGF525');
-
 // track google events 
 // image download with image size 
 function trackImageDownload(imageId, imageSize,imageExportRatio) {
     dataLayer.push({
-        'event': 'imageDownload',
+        'event': 'Image_Download',
         'imageId': imageId,
         'imageSize': imageSize,
         'currentRatio': imageExportRatio
