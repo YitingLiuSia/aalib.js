9ebdbc5ba62e6c8e87f0a4c4bd93e68b187938f7
yitingliu <yiting.liu@sia-partners.com>
2024-07-17 14:37:25 -0400

    update the library resource

--- src/renderers/HTMLRenderer.js
+++ src/renderers/HTMLRenderer.js
@@ -15,44 +15,60 @@ export class HTMLRenderer extends BaseRenderer {
             tagName: "pre",
             fontSize: 7,
             background: "#fff",
-            color: "#000",
-            gradient: null
-        }, options));
+            color: "#000"
+                }, options));
 
         this.el = this.options.el || document.createElement(this.options.tagName);
         this.el.style.fontSize = this.options.fontSize + "px";
         this.el.style.fontFamily = this.options.fontFamily;
         this.el.style.backgroundColor = this.options.background;
+        this.el.style.color = this.options.color;
 
+        // if (this.options.gradient) {
+        //     this.createGradient();
+        // } else {
+        //     this.el.style.backgroundColor = this.options.background;
+        // }
     }
     render(image) {
         super.render(image);
+        // this.el.style.color = this.gradient;
 
-        if (!image.meta.colored) {
-            this.el.style.color = this.options.gradient;
-        }
-        const renderer = colorRenderer(this.el);
-        //  image.meta.colored
+        // if (!image.meta.colored) {
+        //     this.el.style.color = this.options.color;
+        // }
+        const renderer = monoRenderer(this.el);
+
+        // const renderer =  image.meta.colored
         //     ? colorRenderer(this.el)
         //     : monoRenderer(this.el);
+            // when it is not colored, it is mono 
         return renderer(image);
     }
 
-    // issue with this function 
     createGradient(){
-        const gradient = this.ctx.createLinearGradient(0, 0, this.el.width, this.el.height);
-        this.options.gradient.forEach((color, index) => {
-            gradient.addColorStop(index / (this.options.gradient.length - 1), color);
-        });
+        const gradient = this.options.gradient.map((color, index) => {
+            return `${color} ${(index / (this.options.gradient.length - 1)) * 100}%`;
+        }).join(', ');
+    
         this.gradient = gradient;
-        console.log("create gradient in html ",gradient);
+
+        // this.el.style.color = `linear-gradient(0deg, ${gradientStops})`;
+        console.log("color in create gradient is ",gradient);
+    
+        // const gradient = this.ctx.createLinearGradient(0, 0, this.el.width, this.el.height);
+        // this.options.gradient.forEach((color, index) => {
+        //     gradient.addColorStop(index / (this.options.gradient.length - 1), color);
+        // });
+        // this.gradient = gradient;
+        // console.log("create gradient in HTML RENDERER ",gradient);
 
     }
 }
 
 function colorRenderer(el) {
-    const wrapInColor = ({ char, r, g, b }) => (
-        `<span style="color: rgb(${ r }, ${ g }, ${ b })">${ char }</span>`
+    const wrapInColor = ({ char, r, g, b , a }) => (
+        `<span style="color: rgba(${ r }, ${ g }, ${ b }, ${ a })">${ char }</span>`
     );
 
     const toString = stringRenderer(wrapInColor);
